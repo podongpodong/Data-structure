62d711dd093ce935e5a8039c12ce49de3c21927d
podongpodong <112227715+podongpodong@users.noreply.github.com>
2022-08-27 12:56:44 +0900

    verson2
    Add remove function, search function
    삭제함수 추가, 리스트를 조회하는 함수를 추가하여 탐색 방식 변경

--- linkedlist.c
+++ linkedlist.c
@@ -11,33 +11,69 @@ typedef struct _node{
 
 typedef struct LinkedList{
     Node *head;
-    Node *tail;
+    Node *cur;
     Node *before;
     //int numOfdata;
 }DList;
 
-void Listinit(DList *list);
-void Insert(DList *list, int data);
-void func(DList *list);
+void Listinit(DList *list); //리스트 초기화
+void Insert(DList *list, int data); //데이터 삽입
+int LFirst(DList *list, int *pdata);
+int LNext(DList *list, int *pdata);
+int Remove(DList *list);
 
 int main()
 {
     DList list;
-    //int data;
+    int data = 0;
 
     Listinit(&list);
 
     for(int i = 1; i<6; i++) Insert(&list, i);
 
-    func(&list);
+    if(LFirst(&list, &data))
+    {
+        printf("%d->", data);
 
+        while(LNext(&list, &data))
+        {
+            printf("%d->", data);
+        }
+        printf("NULL\n");
+    }
+
+    if(LFirst(&list, &data))
+    {
+        if(data == 3) Remove(&list);
+
+        while(LNext(&list, &data))
+        {
+            if(data == 3)
+            {
+                Remove(&list);
+                break;
+            }
+        }
+    }
+
+    if(LFirst(&list, &data))
+    {
+        printf("%d->", data);
+
+        while(LNext(&list, &data))
+        {
+            printf("%d->", data);
+        }
+        printf("NULL\n");
+    }
+    return 0;
 }
 
 void Listinit(DList *list)
 {
     list -> head = (Node*)malloc(sizeof(Node));
     list -> head -> next = NULL;
-    list -> tail = NULL;
+    list -> cur = NULL;
     list -> before = NULL;
     //numOfdata = 0;
 }
@@ -50,18 +86,50 @@ void Insert(DList *list, int data)
     newnode -> next = list -> head -> next;
     list -> head -> next = newnode;
 
-    printf("%d\n", list->head->next->data);
+    //printf("%d\n", list->head->next->data); //데이터가 제대로 들어갔는지 확인하는 부분
 
 }
 
-void func(DList *list)
+int LFirst(DList *list, int *pdata) 
+/* 
+데이터 참조를 위한 초기화, 첫번째 데이터 pdata가 가리키는 메모리에 저장
+참조 성공 시 TURE 실패 시 FALSE
+*/
 {
-    Node *cur = list -> head -> next;
+    if(list -> head -> next == NULL)
+        return FALSE;
+    
+    list -> cur = list -> head -> next;
+    list -> before = list -> head;
+    *pdata = list -> cur -> data;
+    
+    return TRUE;
+}
 
-    while(cur != NULL)
-    {
-        printf("%d->", cur -> data);
-        cur = cur -> next;
-    }
-    printf("NULL\n");
+int LNext(DList *list, int *pdata)
+/*
+LFirst 함수에서 참조한 다음 노드의 데이터를 pdata에 저장, 연속적으로 실행가능
+처음부터 다시 참조하게 될 시 LFirst함수 호출
+*/
+{
+    if(list -> cur -> next == NULL)
+        return FALSE;
+
+    list -> before = list -> cur;
+    list -> cur = list -> cur -> next;
+    *pdata = list -> cur -> data;
+
+    return TRUE;
+}
+
+int Remove(DList *list)
+{
+    Node *rnode = list -> cur;
+    int rdata = rnode->data;
+    
+    list -> before -> next = list -> cur -> next; 
+    list -> cur = list -> before;
+    free(rnode);
+
+    return rdata;
 }
