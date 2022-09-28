816039e6b02a260c126e7f2f6fcb3291845319d8
podongpodong <112227715+podongpodong@users.noreply.github.com>
2022-09-28 19:52:55 +0900

    tree Version2
    레벨 탐색 추가
    노드 삭제기능 미구현

--- tree.c
+++ tree.c
@@ -19,13 +19,45 @@
  void DeleteNode(Node *mt);
  void DeleteTree(Node *mt);
 
- void AddChild(Node *parentNode, Node *childNode);
-void PreorderTraversal(Node *mt);
+void AddChild(Node *parentNode, Node *childNode);
+void Search(Node *root, int level);
 
- int main()
- {
+int main()
+{
+    Node *root = SetNode('A');
 
- }
+    Node *B = SetNode('B');
+    Node *C = SetNode('C');
+    Node *D = SetNode('D');
+    Node *E = SetNode('E');
+    Node *F = SetNode('F');
+    Node *G = SetNode('G');
+    Node *H = SetNode('H');
+    Node *I = SetNode('I');
+    Node *J = SetNode('J');
+    Node *K = SetNode('K');
+
+    AddChild(root, B);
+    AddChild(B, C);
+    AddChild(B, D);
+    AddChild(D, E);
+    AddChild(D, F);
+
+    AddChild(root, G);
+    AddChild(G, H);
+
+    AddChild(root, I);
+    AddChild(I, J);
+    AddChild(J, K);
+
+    for(int i = 0; i<4; i++)
+    {
+        Search(root, i);
+        printf("\n");
+    }
+    
+
+}
 
 // process of creating new node is same as binary tree
 Node* SetNode(TData data)
@@ -65,4 +97,31 @@ void AddChild(Node *parentNode, Node *childNode)
         }
         tmp->rightSibling = childNode;
     }
+}
+
+//형제노드에 자식이 있는 경우는 탐색 불가
+//반복문으로는 모든 노트 탐색에 제한
+//재귀함수 필요
+void Search(Node *node, int level)
+{
+    if(level > 0)
+    {
+        if(node->leftChild != NULL)
+        {
+            Search(node->leftChild, level-1);
+        }
+        if(node->rightSibling != NULL)
+        {
+            Search(node->rightSibling, level);
+        }
+    }
+    else
+    {
+        while(node != NULL)
+        {
+            printf("%c - ", node->data);
+            node = node->rightSibling;
+        }
+    }
+
 }
