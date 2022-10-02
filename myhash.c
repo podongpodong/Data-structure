42b66e08c6935dc4f4b7bec03e974e701eceaa7c
podongpodong <112227715+podongpodong@users.noreply.github.com>
2022-10-02 15:22:38 +0900

    아직도 개선중인 해시 테이블
    삭제함수 추가
    특정 Bucket만 출력가능하게 하는 함수 추가

--- myhash.c
+++ myhash.c
@@ -1,26 +1,32 @@
-//HashTable 구현
-//hash Collision은 무시
-//학번 이름을 저장하는 해시테이블
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #define BUCKET_SIZE 10
 
-typedef struct bucket{
+typedef struct _node{
     int key; //학번
     char value[20]; //이름
+    struct _node *next;
+}Node;
+
+typedef struct _bucket{
+    int numOfdata; // 같은 색인의 데이터가 몇개 있는지 확인하는 용도
+    Node *head;// 충돌이 일어나는 데이터를 연결리스트로 연결
 }Bucket;
 
+Node* CreateNode(int key, char *value);
 int Hashing(int key);
-void Add(Bucket **tb, int key, char *value);
-void Delete(Bucket **tb, int key);
+void Init(Bucket *hashtable);
+void Add(Bucket *hashtable, int key, char *value);
+void Delete(Bucket *hashtable, int key);
+void Display_Bucket(Bucket *hashtable, int key);
+void Display_All(Bucket *hashtable);
 
 int main()
 {
-    Bucket *hashtable[BUCKET_SIZE];
-    for(int i = 0; i<BUCKET_SIZE; i++) hashtable[i] = NULL;
-    
+    Bucket hashtable[BUCKET_SIZE];
+    Init(hashtable);
+
     int num;
     scanf("%d", &num);
 
@@ -33,26 +39,17 @@ int main()
         Add(hashtable, number, name);
     }
 
-    for(int i = 0; i<BUCKET_SIZE; i++)
-    {
-        if(hashtable[i] != NULL)
-        {
-            printf("학번 : %d 이름 : %s\n", hashtable[i]->key, hashtable[i]->value);
-        }
-    }
+    Display_All(hashtable);
+}
 
-    int a;
-    scanf("%d", &a);
-    Delete(hashtable, a);
+Node* CreateNode(int key, char *value)
+{
+    Node *newnode = (Node*)malloc(sizeof(Node));
+    newnode->key = key;
+    strcpy(newnode->value, value);
+    newnode->next = NULL;
 
-    for(int i = 0; i<BUCKET_SIZE; i++)
-    {
-        if(hashtable[i] != NULL)
-        {
-            printf("학번 : %d 이름 : %s\n", hashtable[i]->key, hashtable[i]->value);
-        }
-    }
-    
+    return newnode;
 }
 
 int Hashing(int key)
@@ -60,21 +57,51 @@ int Hashing(int key)
     return key%BUCKET_SIZE;
 }
 
-void Add(Bucket **tb, int key, char *value)
+void Init(Bucket *hashtable)
 {
-    Bucket *nb = (Bucket*)malloc(sizeof(Bucket));
-    nb->key = key;
-    strcpy(nb->value, value);
-
-    int idx = Hashing(key);
-    tb[idx] = nb;
+    for(int i = 0; i<BUCKET_SIZE; i++) 
+        hashtable[i].head = NULL;
 }
 
-void Delete(Bucket **tb, int key)
+void Add(Bucket *hashtable, int key, char *value)
 {
     int idx = Hashing(key);
-    
-    Bucket *rnode = tb[idx];
-    tb[idx] == NULL;
-    free(rnode);
+    Node *input = CreateNode(key,value);
+
+    if(hashtable[idx].head == 0)
+    {
+        hashtable[idx].head = input;
+    }
+    else
+    {
+        Node *cur = hashtable[idx].head;
+        while(cur->next != NULL)
+            cur = cur->next;
+        
+        cur->next = CreateNode(key,value);
+
+    }
+
+}
+
+void Delete(Bucket *hashtable, int key)
+{
+}
+
+void Display_All(Bucket *hashtable)
+{
+    for(int i = 0; i<BUCKET_SIZE; i++)
+    {
+        if(hashtable[i].head != NULL)
+        {
+            Node *cur = hashtable[i].head;
+            printf("key : %d ", cur->key);
+            while(cur != NULL)
+            {
+                printf(" name : %s", cur->value);
+                cur = cur->next;
+            }
+            printf("\n");
+        }
+    }
 }
